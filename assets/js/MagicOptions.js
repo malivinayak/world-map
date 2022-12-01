3b7d630cdfff8394050f6552f2876456eb28a268
Vinayak Mali <malivinayak012@gmail.com>
2022-12-01 20:20:51 +0530

    updating magic mouse options

---
+++ assets/js/MagicOptions.js
@@ -0,0 +1,9 @@
+options = {
+    "cursorOuter": "circle-basic",
+    "hoverEffect": "circle-move",
+    "hoverItemMove": false,
+    "defaultCursor": false,
+    "outerWidth": 31,
+    "outerHeight": 31
+};
+magicMouse(options);
